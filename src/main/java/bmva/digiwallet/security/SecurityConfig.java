53f90df9bc42856b18bfe3f86fb487a47e6e2a5d
braulio valdes antiqueo <brauliomariano@gmail.com>
2024-06-14 01:36:28 -0400

    corrige error clase bootstrap login-error-inicio

--- src/main/java/bmva/digiwallet/security/SecurityConfig.java
+++ src/main/java/bmva/digiwallet/security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package bmva.digiwallet.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -34,4 +39,15 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Or "*" for any origin
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST")); // Specify allowed HTTP methods
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
 }
