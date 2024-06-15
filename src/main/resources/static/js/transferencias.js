eaa9017369ce6ae53c8a906f490d2f21993e820a
braulio valdes antiqueo <brauliomariano@gmail.com>
2024-06-15 15:10:28 -0400

    agrega tasa conversion clp-usd

--- src/main/resources/static/js/transferencias.js
+++ src/main/resources/static/js/transferencias.js
@@ -11,24 +11,38 @@ $(document).ready(function() {
 			$('#cuentasterceros').show();
 		}
 	});
-
-	$.getJSON('https://mindicador.cl/api', function(data) {
-		var dailyIndicators = data;
+	
+	// tasas de conversion monetaria	
+	$.getJSON('https://v6.exchangerate-api.com/v6/279566e6b5cbe95619058dca/pair/CLP/USD', function(data) {
+		var tasa = data;	
 		let fechaHoy = new Date();
 		let dia = fechaHoy.getDate().toString().padStart(2, '0');
 		let mes = (fechaHoy.getMonth() + 1).toString().padStart(2, '0'); // Enero es 0
 		let año = fechaHoy.getFullYear();
+		$("<p/>", {
+			html: '<h3>Tasa Peso Chileno a Dolar</h3><h4>'+
+			'Fecha de Hoy '+`${dia}/${mes}/${año}`+
+			'<table class="table table-align-center table-striped table-bordered">'+
+			'<tr><td>CLP->USD ' + tasa.conversion_rate+'</td></tr>'+
+			'</table></h4>'
+		}).appendTo(".tasas");
+	}).fail(function() {
+		console.log('Error al consumir la API!');
+	});
+	
+	// indicadores económicos
+
+	$.getJSON('https://mindicador.cl/api', function(data) {
+		var dailyIndicators = data;
 		
 		$("<p/>", {
-			html: '<h2>Indicadores Económicos</h2><h4>'+
-			'Fecha de Hoy '+`${dia}/${mes}/${año}`+
+			html: '<h3>Indicadores Económicos</h3><h5>'+
 			'<table class="table table-align-center table-striped table-bordered">'+
 			'<tr><td>valor Dolar $ ' + dailyIndicators.dolar.valor+'</td></tr>'+
 			'<tr><td>valor Euro  $ ' + dailyIndicators.euro.valor+'</td></tr>'+
 			'<tr><td>valor U.F.  $ ' + dailyIndicators.uf.valor+'</td></tr>'+
 			'<tr><td>valor UTM   $ ' + dailyIndicators.utm.valor+'</td></tr>'+
-			'<tr><td>valor IPC   % ' + dailyIndicators.ipc.valor+'</td></tr>'+
-			'</table></h4>'
+			'</table></h5>'
 		}).appendTo(".indicadores");
 	}).fail(function() {
 		console.log('Error al consumir la API!');
